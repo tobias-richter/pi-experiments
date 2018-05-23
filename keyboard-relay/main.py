3fa46eba357b43b0552fcce841bfd319389e84a1
Tobias Richter <richter.tobias@gmx.de>
2018-05-24 00:29:08 +0200

    add working relais with dirty but working waterflow sensor and pouring of specified ml

--- keyboard-relay/main.py
+++ keyboard-relay/main.py
@@ -33,6 +33,26 @@ relais = [
     relais8
 ]
 
+FLOW_SENSOR = 21
+
+global count
+count = 0
+global countsPer100Ml
+countsPer100Ml = 570
+
+global pourMl
+pourMl = None
+
+def countPulse(channel):
+    global count
+    count = count+1
+    #print count
+
+
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
+
+
 def main(stdscr):
     # do not wait for input when calling getch
     stdscr.nodelay(1)
@@ -47,10 +67,31 @@ def main(stdscr):
             y+=1
             stdscr.clrtoeol()
 
+        y+=2
+        stdscr.addstr(y, 0, "'r' resets flow sensor and ml to pour, 'a' manual mode, 's'  pours 50ml, 'd' pours 100ml, 'f' pours 200ml",
+                      curses.A_REVERSE)
+        y+=1
+        global count
+        global pourMl
+        pouredMl = count / float(countsPer100Ml) * 100
+        stdscr.addstr(y, 0, "Flowsensor count {0}, ml : {1:.2f}".format(count,pouredMl))
+        stdscr.clrtoeol()
+        y+=1
+        if pourMl is not None:
+            stdscr.addstr(y, 0, "Pour ml: {0}".format(pourMl))
+            stdscr.clrtoeol()
+            if pouredMl >= pourMl:
+                relais[0].off()
+        else:
+            stdscr.addstr(y, 0, "Manual pouring")
+            stdscr.clrtoeol()
+
 
         # get keyboard input, returns -1 if none available
         key = stdscr.getch()
         if (key == ord('1')):
+            if relais[0].isOff():
+                count = 0
             relais[0].toggle()
         if (key == ord('2')):
             relais[1].toggle()
@@ -66,6 +107,16 @@ def main(stdscr):
             relais[6].toggle()
         if (key == ord('8')):
             relais[7].toggle()
+        if key == ord('r'):
+            count = 0
+        if key == ord('a'):
+            pourMl = None
+        if key == ord('s'):
+            pourMl = 50
+        if key == ord('d'):
+            pourMl = 100
+        if key == ord('f'):
+            pourMl = 200
         if key == ord('q'):
             break
         #if c != -1:
